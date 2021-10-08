4d7842c331ccc2a545880d6cf1c82924b6f7e714
alex <format37@gmail.com>
2021-10-08 12:51:56 +0300

    deleted:    1c/catboost.cf 	deleted:    1c/ir_console_code.f1c 	deleted:    1c/ir_skd_scheme.xml 	deleted:    1c/ir_skd_settings.xml 	deleted:    cgi-bin/feature_importance.py 	deleted:    cgi-bin/predict_ml.py 	deleted:    cgi-bin/server.py 	deleted:    cgi-bin/train_debug.ipynb 	deleted:    cgi-bin/train_ml.py 	deleted:    mount.sh 	deleted:    start.sh

--- mount.sh
+++
@@ -1,1 +0,0 @@
-mount -t cifs //10.2.4.88/1c_map_uid /mnt/share/1c_map_uid -o username=USER,password=PWD,domain=icecorp.ru
--- start.sh
+++
@@ -1,2 +0,0 @@
-cd /home/alex/projects/1c_ml_regression_diagnostics
-python3 -m http.server --cgi 8000
